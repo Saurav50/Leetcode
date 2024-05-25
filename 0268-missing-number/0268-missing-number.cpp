ae37b88c39cb493967f8ab33e7208c23d80a2266
Saurav Kumar Jha <98273408+Saurav50@users.noreply.github.com>
2024-05-25 22:12:48 +0530

    Time: 13 ms (59.77%), Space: 20.2 MB (98.33%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0;i<=n;i++){
-            bool check=false;
-            for(int j=0;j<n;j++){
-                if(nums[j]==i) {
-                    check=true;
-                    continue;
-                }
-            }
-            if(check==false) return i;
-        }
-          return -1; 
+//     int missingNumber(vector<int>& nums) {
+//         int n=nums.size();
+//         for(int i=0;i<=n;i++){
+//             bool check=false;
+//             for(int j=0;j<n;j++){
+//                 if(nums[j]==i) {
+//                     check=true;
+//                     continue;
+//                 }
+//             }
+//             if(check==false) return i;
+//         }
+//           return -1; 
     
+//     }
+//     optimal sol
+    int missingNumber(vector<int>& nums){
+        int n=nums.size();
+        int ArraySum=0;
+        for(int i=0;i<n;i++) ArraySum+=nums[i];
+        int originalSum=n*(n+1)/2;
+        return originalSum-ArraySum;
     }
 };
