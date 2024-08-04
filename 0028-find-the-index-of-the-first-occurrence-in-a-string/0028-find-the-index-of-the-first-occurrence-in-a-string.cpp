14de75708ae5d4424b0c0d2ae2aab298eba38fd7
Saurav Kumar Jha <98273408+Saurav50@users.noreply.github.com>
2024-08-04 15:50:03 +0530

    Time: 0 ms (100%), Space: 7.9 MB (24.69%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -6,11 +6,13 @@ public:
         if(n1<n2) return -1;
         if(n1==n2&&haystack==needle) return 0;
         for(int i=0;i<n1-n2+1;i++){
-            string temp="";
+            // string temp="";
             for(int j=0;j<n2;j++){
-                temp+=haystack[i+j];
+                // temp+=haystack[i+j];
+                if(haystack[i+j]!=needle[j]) break;
+                if(j==n2-1) return i;
             }
-            if(temp==needle) return i;
+            // if(temp==needle) return i;
 
         }
         return -1;
