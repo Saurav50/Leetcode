b3f7e72768f03966c825dff58edbb98f12991b70
Saurav Kumar Jha <98273408+Saurav50@users.noreply.github.com>
2024-08-24 00:07:15 +0530

    Time: 72 ms (62.82%), Space: 102.3 MB (53.84%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -5,26 +5,12 @@ public:
         int i = 0;
         int j = 1;
         int count = 0;
-        deque<int> dq;
-        int check = true;
-        int indx = INT_MIN;
         queue<int> q;
 
-        dq.push_back(colors[0]);
-       
-        j = 1;
+        while (i < colors.size()) {
 
-        while (j < colors.size() + k - 1) {
-
-            if (dq.size() && (dq.back() == colors[j % colors.size()] ||
-                              dq.front() == dq.back())) {
-                q.push(j - 1);
-            }
-
-            dq.push_back(colors[j % colors.size()]);
-
-            if (dq.size() == 3) {
-                dq.pop_front();
+            if (colors[(j - 1) % colors.size()] == colors[j % colors.size()]) {
+                q.push((j) % colors.size());
             }
 
             if (j - i + 1 < k) {
@@ -32,14 +18,16 @@ public:
             }
 
             else if (j - i + 1 == k) {
-
                 if (q.front() == i) {
-                    q.pop();
+                            q.pop();
                 }
-
                 if (q.size() == 0) {
                     count++;
                 }
+                
+                
+                
+
                 i++;
                 j++;
             }
