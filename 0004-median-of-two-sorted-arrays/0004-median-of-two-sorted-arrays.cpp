ce70a954281147b771f25f8b6aef6c0ebee779c6
Saurav Kumar Jha <98273408+Saurav50@users.noreply.github.com>
2024-09-15 01:54:31 +0530

    Time: 20 ms (78.84%), Space: 94.8 MB (71.08%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // DONT STORE WHOLE MERGED ARRAY JUST PLAY WITH POINTERS
-        int i=0,j=0;
-        int countIdx=0;
-        int n=nums1.size()+nums2.size();
-        int idx1=n/2-1;
-        int idx2=n/2;
-        int ele1=-1,ele2=-1;
-        
-        while(i<nums1.size()&&j<nums2.size()){
-            if(nums1[i]<nums2[j]){
-                if(countIdx==idx1) ele1=nums1[i];
-                if(countIdx==idx2) ele2=nums1[i];
-                countIdx++;
-                i++;
-            }else{
-                if(countIdx==idx1) ele1=nums2[j];
-                if(countIdx==idx2) ele2=nums2[j];
-                countIdx++;
-                j++;
+        if(nums1.size()>nums2.size()){
+             return findMedianSortedArrays(nums2,nums1);
+        }
+        int m=nums1.size();
+        int n=nums2.size();
+        int i=0,j=m;
+        while(i<=j){
+            int Px=(i+j)/2; // from nums1;
+            int Py=(m+n+1)/2-Px; //from nums2;
+            int x1=(Px==0)? INT_MIN:nums1[Px-1];
+            int x2=(Py==0)? INT_MIN:nums2[Py-1];
+            int x3=(Px==m)? INT_MAX:nums1[Px];
+            int x4=(Py==n)? INT_MAX:nums2[Py];
+
+            if(x1<=x4&&x2<=x3){
+                if((m+n)%2==1) return (double)max(x1,x2);
+                return (double)(max(x1,x2)+min(x3,x4))/2;
             }
+            if(x1>x4){
+                j=Px-1;
+            }else i=Px+1;
+
         }
-        while(i<nums1.size()){
-             if(countIdx==idx1) ele1=nums1[i];
-                if(countIdx==idx2) ele2=nums1[i];
-                countIdx++;
-                i++;
-        }
-        while(j<nums2.size()){
-            if(countIdx==idx1) ele1=nums2[j];
-                if(countIdx==idx2) ele2=nums2[j];
-                countIdx++;
-                j++;
-        }
-        if(n%2==0) return (double)(ele1+ele2)/(double)2;
-        return ele2;
-        
-        
-        
+        return -1;
     }
 };
