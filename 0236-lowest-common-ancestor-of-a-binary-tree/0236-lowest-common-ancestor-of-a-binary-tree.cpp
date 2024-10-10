906da7385ad058bbbfd601025d435e87546b442b
Saurav Kumar Jha <98273408+Saurav50@users.noreply.github.com>
2024-10-10 23:59:43 +0530

    Time: 16 ms (28.69%), Space: 16.3 MB (51.48%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,32 +9,15 @@
  */
 class Solution {
 public:
-    bool genPath(TreeNode* root,vector<TreeNode*>&path,TreeNode* k){
-        if(root==NULL) return false;
-        if(root==k){
-            path.push_back(root);
-            return true;
-        }
-        path.push_back(root);
-        if(genPath(root->left,path,k)||genPath(root->right,path,k)){
-            return true;
-        }
-        path.pop_back();
-        return false;
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> pathP;
-        vector<TreeNode*> pathQ;
-        genPath(root,pathP,p);
-        genPath(root,pathQ,q);
-        int i = 0;
-        while (i < pathP.size() && i < pathQ.size() && pathP[i] == pathQ[i]) {
-            i++;
-        }
-
-        // The LCA is the last common node
-        return pathP[i - 1];
-       
-
+        if(root==NULL) return NULL;
+        if(root==p||root==q) return root;
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        if(left==NULL&&right==NULL) return NULL;
+        if(left!=NULL&&right!=NULL) return root;
+        if(left==NULL&&right!=NULL) return right;
+        if(left!=NULL&&right==NULL) return left;
+        return NULL;
     }
 };
