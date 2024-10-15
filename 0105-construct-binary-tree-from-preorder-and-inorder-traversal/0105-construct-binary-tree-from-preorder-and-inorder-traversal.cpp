1d6300080c9445985d78e4db5a710e7267539182
Saurav Kumar Jha <98273408+Saurav50@users.noreply.github.com>
2024-10-16 00:53:15 +0530

    Time: 11 ms (72.69%), Space: 27.4 MB (56.01%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,49 +11,26 @@
  */
 class Solution {
 public:
-    // Tree construct using inorder Nd preOrder
-    TreeNode* ConstructTreePre(vector<int>& preOrder, vector<int>& inOrder,int inS,int inE,int preS,int preE){
-        if (inS > inE || preS>preE) return NULL;
+    TreeNode* buildTreeCall(vector<int>& preorder,int ps,int pe,vector<int>& inorder,int is,int ie,map<int,int> &inorderMap){
+        if (ps > pe || is > ie) return NULL;
 
-        TreeNode *root=new TreeNode(preOrder[preS]);
-        int rootData=preOrder[preS];
-        int rootIndex=-1;
-        for(int i=inS;i<=inE;i++){
-            if(inOrder[i]==rootData){
-                rootIndex=i;
-                break;
-            }
-        }
-
-        //construct left subtree
-
-        int LinS=inS;
-        int LinE=rootIndex-1;
-        int LpreS=preS+1;
-        int LpreE=LpreS+(LinE-LinS);
-        TreeNode *leftChild=ConstructTreePre(preOrder,inOrder,LinS,LinE,LpreS,LpreE);
-        root->left=leftChild;
-
-        //construct right subtree
-        int RinS=rootIndex+1;
-        int RinE=inE;
-        int RpreS=LpreE+1;
-        int RpreE=preE;
-        TreeNode *rightChild=ConstructTreePre(preOrder,inOrder,RinS,RinE,RpreS,RpreE);
-        root->right=rightChild;
+        TreeNode* root = new TreeNode(preorder[ps]);
+        int rootidx=inorderMap[preorder[ps]];
+        TreeNode* leftSubTree=buildTreeCall(preorder,ps+1,ps+rootidx-is,inorder,is,rootidx-1,inorderMap);
+        TreeNode* rightSubTree=buildTreeCall(preorder,ps+rootidx-is+1,pe,inorder,rootidx+1,ie,inorderMap);
+        root->left=leftSubTree;
+        root->right=rightSubTree;
 
         return root;
-
-
     }
-
-    
-    
-    
-    
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int size=preorder.size();
-        return ConstructTreePre(preorder,inorder,0,size-1,0,size-1);
+        if(preorder.size()==0) return NULL;
+        map<int,int> inorderMap;
+        for(int i=0;i<inorder.size();i++){
+            inorderMap[inorder[i]]=i;
+        }
+        TreeNode* root=buildTreeCall(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,inorderMap);
+        return root;
         
     }
 };
