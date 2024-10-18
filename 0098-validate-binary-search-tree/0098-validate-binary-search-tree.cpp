dfd1d407f64d412d2973923e0b1f6a76c5c69829
Saurav Kumar Jha <98273408+Saurav50@users.noreply.github.com>
2024-10-18 23:27:51 +0530

    Time: 0 ms (100%), Space: 20.5 MB (51.2%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,13 +11,23 @@
  */
 class Solution {
 public:
-bool isValidBSTHelper(TreeNode* root, long low, long high) {
-    if (root == NULL) return true;
-    if (root->val <= low || root->val >= high) return false;
-    return isValidBSTHelper(root->left, low, root->val) && isValidBSTHelper(root->right, root->val, high);
-}
+    void genInorder(TreeNode* root,vector<int> &in){
+        if(root==NULL) return;
 
-bool isValidBST(TreeNode* root) {
-    return isValidBSTHelper(root, LONG_MIN, LONG_MAX);
-}
+        if(root->left) genInorder(root->left,in);
+        in.push_back(root->val);
+        if(root->right) genInorder(root->right,in);
+    }
+    bool isValidBST(TreeNode* root) {
+        vector<int> in;
+        genInorder(root,in);
+        bool check=true;
+        for(int i=1;i<in.size();i++){
+            if(in[i]<=in[i-1]){
+                check=false;
+                break;
+            }
+        }
+        return check;
+    }
 };
