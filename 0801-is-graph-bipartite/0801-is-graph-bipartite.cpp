003d303c94293e1d7d25b03df7871484dbcce8f5
Saurav Kumar Jha <98273408+Saurav50@users.noreply.github.com>
2024-12-04 20:34:51 +0530

    Time: 0 ms (100%), Space: 17 MB (22.83%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,19 +1,13 @@
 class Solution {
-    bool check(vector<vector<int>>& graph,int start,vector<int>& colored){
+    bool check(vector<vector<int>>& graph,int start,vector<int>& colored,int node){
         colored[start]=1;
-        queue<int> q;
-        q.push(start);
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            // traverse neighbors
-            for(auto const& it:graph[node]){
+        // traverse neighbors
+        for(auto const& it:graph[node]){
                 if(colored[it]==-1){
                     colored[it]=!colored[node];
-                    q.push(it);
+                    if(!check(graph,start,colored,it)) return false;
                 }
                 else if(colored[it]==colored[node]) return false;
-            }
         }
         return true;
     }
@@ -22,7 +16,7 @@ public:
         vector<int> colored(graph.size(),-1);
         for(int i=0;i<graph.size();i++){
             if(colored[i]==-1){
-                if(!check(graph,i,colored)){
+                if(!check(graph,i,colored,i)){
                     return false;
                 }
             }
